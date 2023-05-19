6925b51614a9038e769afc3fe75770a93b355129
kdany25 <kabadany25@gmail.com>
2023-05-19 19:46:32 +0200

    initial  commit

---
+++ src/main/java/com/api/springBootMovieApi/SpringBootMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.springBootMovieApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMovieApiApplication.class, args);
+	}
+
+}
