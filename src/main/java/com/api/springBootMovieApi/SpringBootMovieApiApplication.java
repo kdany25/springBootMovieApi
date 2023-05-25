dc86c0e7cd98b090bd37fa3495aa5648829b949b
kdany25 <kabadany25@gmail.com>
2023-05-25 08:55:38 +0200

    added cors

--- src/main/java/com/api/springBootMovieApi/SpringBootMovieApiApplication.java
+++ src/main/java/com/api/springBootMovieApi/SpringBootMovieApiApplication.java
@@ -2,12 +2,27 @@ package com.api.springBootMovieApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootMovieApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMovieApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
